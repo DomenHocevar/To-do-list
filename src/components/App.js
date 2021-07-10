48161ab92d72e90a700d4d56b0a172edd078c231
domen <domenhocevar1@gmail.com>
2021-07-10 12:58:45 +0200

    Initialized project

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div id="content"></div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import App from 'components/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
